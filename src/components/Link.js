4f0e4632dd5cdfc508534a9b1358c150af4b83de
Rafael Dantas <rafael.dantas.br@gmail.com>
2018-08-08 14:39:39 -0300

    Todo list files

---
+++ src/components/Link.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+​
+const Link = ({ active, children, onClick }) => {
+  if (active) {
+    return <span>{children}</span>
+  }
+​
+  return (
+    <a
+      href=""
+      onClick={e => {
+        e.preventDefault()
+        onClick()
+      }}
+    >
+      {children}
+    </a>
+  )
+}
+
+export default Link
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-class Todo extends Components {
-	render() {
-		return (
-			<li onClick={onClick} style={{ textDecoration: completed ? 'line-through' : 'none'}}> {text} </li>
-		)
-	}
-}
+const Todo = ({ onClick, completed, text }) => (
+  <li
+    onClick={onClick}
+    style={ {
+      textDecoration: completed ? 'line-through' : 'none'
+    }}
+  >
+    {text}
+  </li>
+)
 
 export default Todo;
